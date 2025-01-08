d8dfe86a7747b8b1a2f82552555b5d338d92c0b5
Garima  Thakur <garimathakur397@gmail.com>
2025-01-08 10:34:57 +0530

    Ktm program

---
+++ KtM.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class KtM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the distance in kilometers: ");
+        double kms = sc.nextDouble();
+		 double miles = kms * 0.621371;
+
+      System.out.println(miles);
+
+         
+    }
+}
